820e928191dcf1f1decdeda5273f07822015a152
Clapfix <ronny.perner@kabelmail.de>
2022-01-25 12:28:24 +0100

    Objekte 1 Daten/Funktionen

--- script_01.js
+++ script_01.js
@@ -1,10 +1,54 @@
 
 /***** Erinnerung: Arrays *******/
 
+// let arr;
+// arr = ["Ich","bin","Max"]
+// output(arr);
+// output(arr[0]);
+// output("------------");
+
+// arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
+// // output(arr[0][2]);
+// // output(arr[1][2]);
+
+// // nested loops
+// for (let i = 0; i < 2; i++) {
+//         for (let j = 0; j < 3; j++){
+//         output(arr[i],[j]);
+//         }
+// }
+                
+  
+
+
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person = 
+        {
+                firstName:"Tilo",
+                lastName:"Schönemeyer",
+                salary:[120000,160000],
+                permission: true,
+                sayHello: 
+                function(){
+                return "Hello, i am " + this.firstName;
+                }
+        
 
+        };
+
+
+// output(person);
+person.firstName = "Thilo";
+// output(person.firstName);
+// output(person.lastName);
+
+const txt =     "Ich bin " + person.firstName + " " + person.lastName +
+                " und verdiene " + person.salary[1] + " p.a."
+output(txt);
+
+output(person.sayHello());
 
 
 /** Ausgabe */
